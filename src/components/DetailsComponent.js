1130c5ca4fdddd03e011d0026dc5742ec9f89361
pjwhite08 <pamelajeanwhite@gmail.com>
2022-01-07 11:29:28 -0600

    started to add map

--- src/components/DetailsComponent.js
+++ src/components/DetailsComponent.js
@@ -1,6 +1,21 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
+
+//withScriptjs will embed the Google script that it needs to load for the map to work correctly 
+import { GoogleMap, withScriptjs, withGoogleMap }  from 'react-google-maps'
+
 //import material UI components
+
+function Map() {
+    return (
+        <GoogleMap 
+            defaultZoom={10} 
+            defaultCenter={{lat: 43.710121, lng:-74.973419}}
+        />
+    )
+}
+
+const WrappedMap = withScriptjs(withGoogleMap(Map));
 
 
 const Details = (props) => {
@@ -9,16 +24,23 @@ const Details = (props) => {
     const listing = props.listings.find(listing => listing.id == id)
 
     return (
-        <main>
-            <div style={{display: "flex", flexDirection: "column", alignItems:"center"}}>
+        <main style={{display:"flex", justifyContent:"center"}}>
+            <div style={{display: "flex", flexDirection: "column", alignItems:"center", backgroundColor:"hotpink", width:"500px"}}>
                 <div>
                     <h1>{listing.name}</h1>
                     <h2>{listing.address}</h2>
                     <h2>{listing.hours}</h2>
                     <p>{listing.description}</p>
                 </div>
-                <div>
-                    this is a map
+                <div style={{width: "", height: ""}}>
+                    {/* to the wrapped map, we need to pass a prop called googleMapURL
+                    eventually an API key will go in the {''} */}
+                    <WrappedMap 
+                        googleMapURL={'https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places'}
+                        loadingElement={<div style={{height:'100%'}}/>}
+                        containerElement={<div style={{height:'100%'}}/>}
+                        mapElement={<div style={{height:'100%'}}/>}
+                        />
                 </div>
             </div>
         </main>
--- src/redux/state.js
+++ src/redux/state.js
@@ -9,7 +9,7 @@ export default {
         {
             id:1, 
             name: "Enchanted Forest Water Safari",
-            description: "this is the description",
+            description: "this is the description.Ham short loin andouille, tail leberkas chuck swine doner. Sirloin jowl landjaeger pastrami boudin pig shoulder ground round shank jerky leberkas rump ribeye filet mignon ham hock. Leberkas meatloaf bacon boudin pork, capicola shankle pork belly jowl. Kevin short ribs spare ribs ham t-bone. Brisket landjaeger ball tip, ham beef swine spare ribs tri-tip pork belly drumstick doner capicola chuck. Biltong short loin pancetta frankfurter kevin chuck venison tail doner brisket turkey kielbasa alcatra cupim porchetta. Bacon tongue cow pig beef ribs alcatra filet mignon burgdoggen rump t-bone turkey ribeye.",
             hours: "10AM-9PM",
             address: "3183 NY-28, Old Forge, NY 13420",
             map: "this is where I figure out google maps, maybe"
